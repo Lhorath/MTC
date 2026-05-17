50cf4fc2009e178d96cffdb1b88d0322e693e938
Zachary MacPhee <zacharymacphee@hotmail.ca>
2026-05-17 11:42:16 -0300

    Update config.php
    w

--- MyTinyCollege/Upgrading/config/config.php
+++ MyTinyCollege/Upgrading/config/config.php
@@ -4,9 +4,9 @@
  */
 return [
     'db' => [
-        'dsn' => 'mysql:host=127.0.0.1;dbname=mytinycollege;charset=utf8mb4',
-        'user' => 'root',
-        'pass' => '',
+        'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=mytinycollege;charset=utf8mb4',
+        'user' => 'website',
+        'pass' => 'dax123',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
